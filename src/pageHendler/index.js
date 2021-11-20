456d78e2d74bb6a86f8b19c6746b289fb4c1651e
Kamil Andziak <kamil.andziakk97@gmail.com>
2021-11-21 00:28:55 +0100

    Project

--- src/pageHendler/index.js
+++ src/pageHendler/index.js
@@ -3,48 +3,21 @@ export default function () {
     console.log('siemaa');
   });
   const main = document.querySelectorAll('main');
-  const currentSectionIndex = 1;
-  let lastScrollTop = 50;
-  const isThrotled = false;
-  document.addEventListener('scroll', () => {
-    const st = document.documentElement.scrollTop;
-    if (st > lastScrollTop) {
-      // downscroll code
-      main[1].scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    } else {
-      // upscroll code
-      main[0].scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
+  let currentSectionIndex = 0;
+  document.addEventListener('wheel', (element) => {
+    const direction = element.deltaY < 0 ? -1 : 1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === main.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
     }
-    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-  }, false);
-  // main[currentSectionIndex].scrollIntoView({
-  //   behavior: 'smooth',
-  //   block: 'start',
-  // });
-
-  // const direction = onwheel(undefined).deltaY < 0 ? -1 : 1;
-  //
-  // // console.log(event.deltaY);
-  // if (direction === 1) {
-  //   const isLastSection = currentSectionIndex === main.length - 1;
-  //   if (isLastSection) return;
-  // } else if (direction === -1) {
-  //   const firstSection = currentSectionIndex === 0;
-  //   if (firstSection) return;
-  // }
-  // currentSectionIndex += direction;
-
-  // console.log(currentSectionIndex);
-
-  // main[currentSectionIndex].scrollIntoView({
-  //   behavior: 'smooth',
-  //   block: 'start',
-  //   });
-  // });
+    currentSectionIndex += direction;
+    console.log(direction);
+    main[currentSectionIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
 }
